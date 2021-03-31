1b964b0235ac83ba65cbe18ba73ce80b9088b9bb
sjoedwards <samuel.j.o.edwards@gmail.com>
2021-03-31 20:10:55 +0100

    stonks

---
+++ TODO.md
@@ -0,0 +1,5 @@
+Refresh token
+
+- [ ] Refresh token spotify
+- [ ] Refresh token strava
+- [ ] Change callback url for strava to be auth=strava
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,56 @@
 import { ReactElement } from "react";
 import { GetServerSideProps } from "next";
+import Link from "next/link";
 import nc from "next-connect";
 import session from "cookie-session";
 import { IncomingMessage } from "http";
 import styles from "../styles/Home.module.css";
 
-// https://www.strava.com/oauth/authorize?client_id=63782&redirect_uri=http://localhost:3000&response_type=code&scope=read_all
-export default function Home(): ReactElement {
-  return <div className={styles.container}>Test</div>;
+interface HomeProps {
+  strava: {
+    loggedIn: boolean;
+  };
+  spotify: {
+    loggedIn: boolean;
+  };
+}
+
+export default function Home({ strava, spotify }: HomeProps): ReactElement {
+  return (
+    <div>
+      {!strava.loggedIn ? (
+        <Link href="https://www.strava.com/oauth/authorize?client_id=63782&redirect_uri=http://localhost:3000&response_type=code&scope=read_all">
+          <button type="button" className={styles.button}>
+            Log In to Strava
+          </button>
+        </Link>
+      ) : (
+        <div>
+          <span>Authenticated with strava</span>
+        </div>
+      )}
+      {!spotify.loggedIn ? (
+        <Link href="https://accounts.spotify.com/authorize?client_id=2980bc524d8249978ce2796c88006256&response_type=code&redirect_uri=http://localhost:3000?auth=spotify&scope=user-read-private%20user-read-email">
+          <button type="button" className={styles.button}>
+            Log In to Spotify
+          </button>
+        </Link>
+      ) : (
+        <div>
+          <span>Authenticated with spotify</span>
+        </div>
+      )}
+    </div>
+  );
 }
 interface Session {
   session?: {
-    accessToken: string;
+    strava?: {
+      accessToken?: string;
+    };
+    spotify?: {
+      accessToken?: string;
+    };
   };
 }
 
@@ -26,7 +65,7 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   try {
     await handler.run(req, res);
     // TODO move the following into an auth middleware as per https://www.npmjs.com/package/next-connect#user-content-runreq-res
-    if (!req.session?.accessToken) {
+    if (!req.session?.strava?.accessToken) {
       if (query.code) {
         const { STRAVA_CLIENT_SECRET, STRAVA_CLIENT_ID } = process.env;
         const token = await fetch(
@@ -35,22 +74,54 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
         );
         const data = await token.json();
         req.session = {
-          accessToken: data.access_token,
+          ...req.session,
+          strava: {
+            accessToken: data.access_token,
+          },
         };
         if (data.errors) {
           throw new Error(JSON.stringify(data.errors));
         }
       }
-    } else {
-      console.log("access token present!");
+    }
+    if (!req.session?.spotify?.accessToken) {
+      if (query.code) {
+        const { SPOTIFY_CLIENT_SECRET, SPOTIFY_CLIENT_ID } = process.env;
+
+        const token = await fetch("https://accounts.spotify.com/api/token", {
+          body: `grant_type=authorization_code&code=${query.code}&redirect_uri=http%3A%2F%2Flocalhost%3A3000%3Fauth%3Dspotify`,
+          headers: {
+            Authorization: `Basic ${Buffer.from(
+              `${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`
+            ).toString("base64")}`,
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          method: "POST",
+        });
+
+        const data = await token.json();
+        req.session = {
+          ...req.session,
+          spotify: {
+            accessToken: data.access_token,
+          },
+        };
+        if (data.errors) {
+          throw new Error(JSON.stringify(data.errors));
+        }
+      }
     }
   } catch (e) {
     console.log("error", e);
   }
-
   return {
     props: {
-      accessToken: "test",
+      strava: {
+        loggedIn: Boolean(req.session?.strava?.accessToken),
+      },
+      spotify: {
+        loggedIn: Boolean(req.session?.spotify?.accessToken),
+      },
     },
   };
 };
